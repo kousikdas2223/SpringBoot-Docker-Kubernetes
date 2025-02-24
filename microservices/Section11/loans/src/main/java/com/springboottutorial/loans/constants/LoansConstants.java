5028aaabdba49f003ff76a24e816ad91e5ad7954
kousikdasaws <kousik.das.aws@gmail.com>
2025-02-24 02:48:00 -0500

    Commit 02242025 0247AM

--- microservices/Section11/loans/src/main/java/com/springboottutorial/loans/constants/LoansConstants.java
+++ microservices/Section11/loans/src/main/java/com/springboottutorial/loans/constants/LoansConstants.java
@@ -15,7 +15,7 @@ public class LoansConstants {
 
 
     public static final String STATUS_201 = "201";
-    public static final String MESSAGE_201 = "Account Created Successfully";
+    public static final String MESSAGE_201 = "Loan Created Successfully";
 
     public static final String STATUS_200 = "200";
     public static final String MESSAGE_200 = "Request Processed Successfully";
